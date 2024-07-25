2a7e606492b611d868d3142720d1400424a2e86f
MohammedFalih <yunusfalih123@gmail.com>
2024-07-25 15:58:41 +0530

    Notification_with_NoticedGem

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -9,10 +9,31 @@ export default class extends Controller {
         event.stopPropagation();
         const commentFormId = event.params["form"];
         const commentBodyId = event.params["body"];
+        const editButtonId = event.params["edit"];
+
         const form = document.getElementById(commentFormId);
+        const commentBody = document.getElementById(commentBodyId)
+        const editButton = document.getElementById(editButtonId)
+
         form.classList.toggle("d-none");
         form.classList.toggle("my-4");
-        const commentBody = document.getElementById(commentBodyId)
         commentBody.classList.toggle("d-none");
+        this.toggleButton(editButton);
+    }
+
+    toggleButton(editButton) {
+        if (editButton.innerText === "Edit") {
+            editButton.innerText = "Cancel"
+            this.toggleEditButtonClass(editButton)
+        }
+        else {
+            editButton.innerText = "Edit"
+            this.toggleEditButtonClass(editButton)
+        }
+    }
+
+    toggleEditButtonClass(editButton) {
+        editButton.classList.toggle("btn-secondary")
+        editButton.classList.toggle("btn-warning")
     }
 }
